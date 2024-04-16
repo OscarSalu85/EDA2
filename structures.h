3ff7e0e9523ea1dd404703bb7525a3f61269ba80
Roigbal2 <157697441+Roigbal2@users.noreply.github.com>
2024-04-16 18:34:30 +0200

    Main Game Structures (MGS)

---
+++ structures.h
@@ -0,0 +1,55 @@
+#ifndef _STRUCTURES_H_
+#define _STRUCTURES_H_
+
+#define MAX_CHAR 100
+#define MAX_ENEMIES 3
+#define MAX_OPTIONS 3
+#define MAX_DECISIONS 3
+//Skills
+typedef struct Skills{
+    char name[MAX_CHAR];
+    char description[MAX_CHAR];
+    char skill[MAX_CHAR];
+    int duration;
+    char modifiers;
+}Skills;
+
+//Character
+typedef struct Character{
+    char name[MAX_CHAR];
+    int hp;
+    int atk;
+    int def;
+    Skills skill[4];
+}Character;
+
+//Enemy
+typedef struct Enemy{
+    char name[MAX_CHAR];
+    int hp;
+    int atk;
+    int def;
+}Enemy;
+
+//Option
+typedef struct Option{
+    char r_text[MAX_CHAR];
+    char n_text[MAX_CHAR];
+    Enemy enemies[MAX_ENEMIES];
+}Option;
+
+//Decision
+typedef struct Decision{
+    char question[MAX_CHAR];
+    Option options[MAX_OPTIONS];
+    int n_options;
+}Decision;
+
+//Scenario
+typedef struct Scenario{
+    char name[MAX_CHAR];
+    char description[MAX_CHAR];
+    Decision decision[];
+}Scenario;
+
+
