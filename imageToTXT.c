f7e586c080c1e07ace8be28b667221a44c6de187
patata85 <oscarrico333@gmail.com>
2024-04-17 17:07:05 +0200

    Bug solved for printColor

--- imageToTXT.c
+++ imageToTXT.c
@@ -61,6 +61,5 @@ void convert(char img[], char dest[],int height, int width){
 
 int main(){
     convert("Image.txt","Final.txt",12,16);
-    convert("Image2.txt","Final2.txt",12,16);
-    printImage("Final.txt")
+    printImage("Final.txt");
 }
--- printPlus.h
+++ printPlus.h
@@ -7,7 +7,7 @@
 
 //Message, F_RGB, B_RGB
 void printColor(char message[],char F_RBG[], char B_RBG[]){
-    printf( "\e[38;2;%sm" "\e[48;2;%sm" "%s" "\e[0m" "\n",F_RBG, B_RBG, message);
+    printf( "\e[38;2;%sm" "\e[48;2;%sm" "%s" "\e[0m",F_RBG, B_RBG, message);
 }
 
 /*
